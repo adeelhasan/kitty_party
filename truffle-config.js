e10d536f69130050612b999344d924609742d0f6
adeelhasan <adeel_hasan@yahoo.com>
2019-06-03 09:48:17 -0400

    removed packages to test locally with ropsten, to simplify local user testing

--- truffle-config.js
+++ truffle-config.js
@@ -18,17 +18,17 @@
  *
  */
 
-require('dotenv').config()
-const HDWalletProvider = require('truffle-hdwallet-provider');
+// require('dotenv').config()
+// const HDWalletProvider = require('truffle-hdwallet-provider');
 
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 // const infuraKey = fs.readFileSync(".infuraKey").toString().trim();
 // let walletOffset = 2;
 
-const mnemonic = process.env.mnemonic;
-const infuraKey = process.env.infuraKey;
-let walletOffset = process.env.walletOffset;
+// const mnemonic = process.env.mnemonic;
+// const infuraKey = process.env.infuraKey;
+// let walletOffset = process.env.walletOffset;
 
 
 module.exports = {
@@ -73,14 +73,14 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`,walletOffset),
-      network_id: 3,       // Ropsten's id
-      gas: 8000000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    }
+    // ropsten: {
+    //   provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`,walletOffset),
+    //   network_id: 3,       // Ropsten's id
+    //   gas: 8000000,        // Ropsten has a lower block limit than mainnet
+    //   confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // }
 
     // Useful for private networks
     // private: {
